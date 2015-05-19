47ce30d2951b580b8a5a7fb2eb8b84f85a1846cf
tom <tom@brak.com>
2015-05-19 21:48:45 +0200

    Asset pipeline for production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,2 @@
 module ApplicationHelper
-  def big_link(image, short_text, explanations)
-  	haml_tag :a, href: '/', class: 'item' do
-  		haml_tag :img, src: image
-  		haml_tag :div, class: 'overlay regular' do
-  			haml_tag :div, class: 'center' do
-  				haml_tag :div, class: 'content' do
-  					haml_concat short_text
-  				end
-  			end
-  		end
-  		haml_tag :div, class: 'overlay detailed' do
-  			haml_tag :div, class: 'center' do
-  				haml_tag :div, class: 'content' do
-		  			explanations.each do |explanation|
-			  			haml_concat explanation
-			  			haml_tag :br
-		  			end
-  				end
-  			end
-  		end
-  	end
-  end
 end
